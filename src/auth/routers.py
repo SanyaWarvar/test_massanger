b1702ec7edec235d6f39e0a6b472d83b03348694
Gladin <danyagladin@gmail.com>
2024-06-12 14:51:00 +0700

    end

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 
 from src.auth.auth_handler import sign_jwt
-from src.auth.utils import pwd_contex
+from secure import pwd_contex
 from src.auth.schemes import UserScheme
 from src.db_manager import DBManager
 from src.database import get_async_session
@@ -39,7 +39,7 @@ async def login(user: UserScheme, session=Depends(get_async_session)):
 
     res = await DBManager.get_user_by_username(session, user.username)
     print(res.hashed_password , pwd_contex.hash(user.password))
-    if not res :
+    if not res or not pwd_contex.verify(user.password, res.hashed_password):
         raise HTTPException(400, "Incorrect data!")
 
     token = sign_jwt(user.username)
